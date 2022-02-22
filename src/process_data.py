75e4bd758ba7328004b9f270d20716e81cfa2ae0
Deris Spina <derisspina@gmail.com>
2022-02-21 22:00:45 -0300

    change variable names

--- src/process_data.py
+++ src/process_data.py
@@ -9,11 +9,19 @@ test_file = 'd00_te.dat' # file name for test data wthout faults
 train_free = pd.read_table(path+train_file,sep='\s+',header=None).T
 # load test data without faults
 test_free = pd.read_table(path+test_file,sep='\s+',header=None)
+old_col_names = list(train_free.columns)
+manipulated = [f'xmv_{i}' for i in range(1,12)]
+measured = [f'xmeas_{i}' for i in range(1,42)]
+new_col_names = measured + manipulated
 
+names_dict = dict(zip(old_col_names,new_col_names))
+train_free.rename(columns = names_dict,inplace=True)
+test_free.rename(columns = names_dict,inplace=True)
 # load test data with faults and add them to an unique dataframe
 test_faulty = None
 for fault_number in range(1,22):
     test_faulty_data = pd.read_table(path+f'd{fault_number:02d}.dat',sep='\s+',header=None)
+    test_faulty_data.rename(columns = names_dict,inplace=True)
     test_faulty_data['fault_number'] = fault_number
     if test_faulty is None:
         test_faulty = test_faulty_data
