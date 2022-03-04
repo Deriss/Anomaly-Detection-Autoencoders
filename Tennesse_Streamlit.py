5d80ed2a55098b5968fdeea646cc08f8e61ab348
Deris Spina <derisspina@gmail.com>
2022-03-04 15:07:44 -0300

    change file paths

--- Tennesse_Streamlit.py
+++ Tennesse_Streamlit.py
@@ -56,13 +56,13 @@ def ROI_curve2(train_mae,test_mae,test_free_mae,plot=True):
     return((false_positive_rate,true_positive_rate))
 ##############
 # Read files
-test_faulty = pd.read_csv('../data/processed/TEP_test_faulty.csv')
-test_free = pd.read_csv('../data/processed/TEP_test_free.csv')
-anomaly = np.genfromtxt('../outputs/Anomaly_4AE.csv', delimiter=',')
-anomaly_free = np.genfromtxt('../outputs/Anomaly_4AE.csv', delimiter=',')
-train_mae = pd.read_csv('../outputs/train_mae_4AE.csv',index_col=0)
-test_mae = pd.read_csv('../outputs/test_mae_4AE.csv',index_col=0)
-test_free_mae = pd.read_csv('../outputs/test_free_mae_4AE.csv',index_col=0)
+test_faulty = pd.read_csv('./data/processed/TEP_test_faulty.csv')
+test_free = pd.read_csv('./data/processed/TEP_test_free.csv')
+anomaly = np.genfromtxt('./outputs/Anomaly_4AE.csv', delimiter=',')
+anomaly_free = np.genfromtxt('./outputs/Anomaly_4AE.csv', delimiter=',')
+train_mae = pd.read_csv('./outputs/train_mae_4AE.csv',index_col=0)
+test_mae = pd.read_csv('./outputs/test_mae_4AE.csv',index_col=0)
+test_free_mae = pd.read_csv('./outputs/test_free_mae_4AE.csv',index_col=0)
 
 # Create variable description 
 continuous = ['A Feed (stream 1)',
